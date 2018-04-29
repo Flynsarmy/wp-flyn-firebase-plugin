880bb6959043209f3748e1e1c52ac6525c71bedb
flynsarmy <flynsarmy@gmail.com>
2018-04-30 09:15:57 +1000

    Generate post list URLs with AJAX so as not to lag the page load

--- flyn-firebase.php
+++ flyn-firebase.php
@@ -9,15 +9,18 @@ Author URI: https://www.flynsarmy.com/
 */
 
 add_filter( 'get_shortlink', function( $url, $post_id = false ) {
+    if ( !defined('FLYN_FIREBASE_URL') || !defined('FLYN_FIREBASE_API_KEY') )
+        return $url;
+
     global $post;
     if ( ! $post_id && $post ) $post_id = $post->ID;
     elseif ( $post_id ) $post = get_post( $post_id );
 
-    if ( $post && $post->post_status != 'publish' )
+    if ( $post && !in_array($post->post_status, ['publish', 'private']) )
         return "";
 
     if ( ( is_singular() || $post ) && ! is_front_page() ) {
-        $shortlink = get_post_meta( $post_id, '_ffirebase_shortlink', true );
+        $shortlink = ffirebase_cached_shortlink($post_id);
         if ( $shortlink )
             return $shortlink;
         // Goo.gl plugin support
@@ -60,6 +63,11 @@ add_filter( 'get_shortlink', function( $url, $post_id = false ) {
     }
 }, 9, 2 );
 
+function ffirebase_cached_shortlink( $post_id )
+{
+    return (string)get_post_meta( $post_id, '_ffirebase_shortlink', true );
+}
+
 function ffirebase_shorten( $url ) {
 	$result = wp_remote_post( add_query_arg( 'key', apply_filters( 'ffirebase_api_key', FLYN_FIREBASE_API_KEY), 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks' ), [
 		'body' => json_encode([
@@ -93,10 +101,87 @@ add_filter( 'manage_edit-post_columns', function( $columns ) {
 
 add_action( 'manage_posts_custom_column', function( $column ) {
     if ( 'shortlink' == $column ) {
-        $shorturl = wp_get_shortlink();
-        $shorturl_caption = preg_replace( '/https?\:\/\//', '', $shorturl );
-        printf( '<a href="%s">%s</a>', esc_url( $shorturl ), esc_html( $shorturl_caption ) );
+        // Don't try to load shortlinks for non-public posts
+        $post_status = get_post_status(get_the_ID());
+        if ( !in_array($post_status, ['publish', 'private']) )
+            return;
+
+        $shorturl = ffirebase_cached_shortlink( get_the_ID() );
+        if ( $shorturl )
+        {
+            $shorturl_caption = preg_replace( '/https?\:\/\//', '', $shorturl );
+            printf( '<a href="%s">%s</a>', esc_url( $shorturl ), esc_html( $shorturl_caption ) );
+        }
+        else
+        {
+            echo "<span class='ffirebase_load_shortlink' data-post_id='".get_the_ID()."'>Generating...</span>";
+        }
     }
+});
+
+add_action('admin_print_footer_scripts-edit.php', function() {
+    ?>
+    <script type="text/javascript">
+        // Grab all IDs to generate shortlink sfor
+        var ffb_shortlink_ids = jQuery('#the-list .column-shortlink:not(.hidden) .ffirebase_load_shortlink').map(function() {
+            return jQuery(this).data('post_id');
+        }).get();
+        // Split into chunks of 5 for AJAX requests
+        var ffb_shortlink_chunks = [];
+        for ( var i = 0; i < ffb_shortlink_ids.length; i+= 5 )
+            ffb_shortlink_chunks.push(ffb_shortlink_ids.slice(i, i+5));
+
+        ffb_get_shortlinks( ffb_shortlink_chunks );
+
+        function ffb_get_shortlinks( chunks )
+        {
+            if ( !chunks.length )
+                return;
+
+            jQuery.ajax({
+                dataType: "json",
+                url: "<?= admin_url("admin-ajax.php?action=ffirebase_generate_shortlinks") ?>",
+                data: jQuery.param({ "ids[]": chunks.shift() }, true),
+                success: function(data, textStatus, jqXHR) {
+                    if ( data.success )
+                    {
+                        for ( var i = 0; i < data.message.length; i++ )
+                            jQuery("#the-list .column-shortlink .ffirebase_load_shortlink[data-post_id="+data.message[i].id+"]").html(
+                                "<a href=\""+data.message[i].url+"\">"+data.message[i].url.replace(/^https?:\/\//, '')+"</a>"
+                            );
+                    }
+
+                    ffb_get_shortlinks(chunks);
+                }
+            });
+        }
+    </script>
+    <?php
+});
+
+add_action('wp_ajax_ffirebase_generate_shortlinks', function() {
+    header('Content-Type: application/json');
+
+    $ids = @$_GET['ids'];
+    if ( !is_array($ids) )
+        exit(json_encode([
+            'success' => 0,
+            'message' => "No post IDs specified.",
+        ]));
+
+    $ids = array_unique(array_map('intval', $ids));
+    $results = [];
+
+    foreach ( $ids as $id )
+        $results[] = [
+            'id' => $id,
+            'url' => wp_get_shortlink($id)
+        ];
+
+    exit(json_encode([
+        'success' => 1,
+        'message' => $results,
+    ]));
 });
 
 add_action( 'save_post', function( $post_ID, $post ) {
@@ -106,3 +191,15 @@ add_action( 'save_post', function( $post_ID, $post ) {
 
     delete_post_meta( $post_ID, '_ffirebase_shortlink' );
 }, 10, 2 );
+
+if ( !defined('FLYN_FIREBASE_URL') || !defined('FLYN_FIREBASE_API_KEY') )
+{
+    add_action('admin_notices', function() {
+        ?>
+        <div class="notice notice-error is-dismissable">
+            <p>Firebase Short Links plugin is enabled but <em>FLYN_FIREBASE_URL</em> or <em>FLYN_FIREBASE_API_KEY </em> constants aren't defined in <em>wp-config.php</em>. Please see the installation instructions section for this plugin.</p>
+        </div>
+        <?php
+
+    });
+}
